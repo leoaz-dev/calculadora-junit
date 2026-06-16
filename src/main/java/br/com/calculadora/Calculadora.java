8372c6ec1633fb67001a2e9fb165bded4e0c1fa1
Leonardo Azevedo <contato.leonardoazevedo@gmail.com>
2026-06-16 13:38:59 -0300

    Implementação classe calculadora

---
+++ src/main/java/br/com/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+package br.com.calculadora;
+
+public class Calculadora {
+
+    public double somar(double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+
+    public double subtrair(double numero1, double numero2) {
+        return numero1 - numero2;
+    }
+
+    public double multiplicar(double numero1, double numero2) {
+        return numero1 * numero2;
+    }
+
+    public double dividir(double numero1, double numero2) {
+        if (numero2 == 0) {
+            throw new ArithmeticException("Não é possível dividir por zero.");
+        }
+
+        return numero1 / numero2;
+    }
+}
